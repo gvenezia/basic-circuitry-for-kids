b029516671aa42e7b667f22f70c2b45ea7c94a31
Gaetano <g.venezia@outlook.com>
2018-02-10 20:42:51 -0600

    added comments to javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,10 +5,12 @@
 		var correctChoice = document.getElementById("correct-choice");
 		var wrongChoice = document.getElementsByClassName("wrong-choice");
 
+		// Assign an event listener to the correct choice
 		correctChoice.addEventListener("click", function(){
 			alert("that's correct!");
 		});
 
+		// Assign event listeners to all of the incorrect choices
 		for (let i = 0; i < wrongChoice.length; i++){
 			wrongChoice[i].addEventListener("click", function(){
 				alert("incorrect");
